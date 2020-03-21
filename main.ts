741ae2c51eaab3e02b01f540690a28133779be1a
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-03-21 13:50:59 +0100

    2 wersja ze sterowaniem diodą laserową

--- main.ts
+++ main.ts
@@ -1,52 +1,38 @@
 input.onButtonPressed(Button.A, function () {
-    jasno = Math.min(jasno + 1, 7)
-    LED.intensity(jasno)
+    brightness = Math.min(brightness + 1, 7)
+    LED.intensity(brightness)
+    pins.analogWritePin(AnalogPin.P12, 1023)
 })
 input.onButtonPressed(Button.B, function () {
-    jasno = Math.max(jasno - 1, 1)
-    LED.intensity(jasno)
+    brightness = Math.max(brightness - 1, 1)
+    LED.intensity(brightness)
+    pins.analogWritePin(AnalogPin.P12, 0)
 })
-let sekunda = 0
-let minuta = 0
-let godzina = 0
 let LED: TM1637.TM1637LEDs = null
-let jasno = 0
-jasno = 1
-basic.showIcon(IconNames.Heart)
-let zegar = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
+let brightness = 0
+brightness = 1
 LED = TM1637.create(
     DigitalPin.P8,
     DigitalPin.P9,
-    jasno,
+    brightness,
     4
 )
-LED.showNumber(8888)
-LED.showDP(5, true)
-basic.pause(1000)
-LED.clear()
-basic.showIcon(IconNames.Yes)
+let zegar = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
 basic.forever(function () {
-    godzina = zegar.getHour()
-    if (godzina > 9) {
-        LED.showbit(Math.floor(godzina / 10), 0)
-        LED.showbit(godzina % 10, 1)
-    } else {
-        LED.showbit(0, 0)
-        LED.showbit(godzina, 1)
-    }
-    minuta = zegar.getMinute()
-    if (minuta > 9) {
-        LED.showbit(Math.floor(minuta / 10), 2)
-        LED.showbit(minuta % 10, 3)
-    } else {
-        LED.showbit(0, 2)
-        LED.showbit(minuta, 3)
-    }
-    sekunda = zegar.getSecond()
-    if (sekunda % 2 == 1) {
+    LED.showbit(Math.floor(zegar.getHour() / 10), 0)
+    LED.showbit(zegar.getHour() % 10, 1)
+    LED.showbit(Math.floor(zegar.getMinute() / 10), 2)
+    LED.showbit(zegar.getMinute() % 10, 3)
+    if (zegar.getSecond() % 2 == 0) {
         LED.showDP(5, true)
     } else {
         LED.showDP(5, false)
     }
+    if (Math.floor(zegar.getSecond() / 10) % 2 == 0) {
+        pins.analogWritePin(AnalogPin.P12, 1023)
+    } else {
+        pins.analogWritePin(AnalogPin.P12, 0)
+    }
     basic.pause(100)
 })
+ 
